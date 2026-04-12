809c41ebfb0f9694e196a2dd2e1f5c1e5bb5fc66
Luis Felipe Patrocinio <patrocinioluisf@gmail.com>
2026-04-12 20:48:26 -0300

    feat(models): add achievement and game details models
    Introduce Achievement and GameDetails classes to represent game metadata and retro achievement data.

---
+++ Models/Achievement.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RetroEncyclopedia.Models {
+    public class Achievement {
+        [JsonPropertyName("ID")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("Title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Points")]
+        public int Points { get; set; }
+
+        [JsonPropertyName("BadgeName")]
+        public string BadgeName { get; set; }
+
+        // Propriedade calculada para montar a URL da imagem da conquista
+        public string BadgeImageUrl {
+            get {
+                return $"https://media.retroachievements.org/Badge/{BadgeName}.png";
+            }
+        }
+    }
+}
---
+++ Models/GameDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RetroEncyclopedia.Models {
+    public class GameDetails {
+        [JsonPropertyName("ID")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("Title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("BoxArt")]
+        public string BoxArtUrl { get; set; }
+
+        [JsonPropertyName("Developer")]
+        public string Developer { get; set; }
+
+        [JsonPropertyName("Achievements")]
+        public List<Achievement> Achievements { get; set; } = new List<Achievement>();
+    }
+}
